1bf32ff15f3ed451bf1beeca435025d97c2ad47d
Mauro <mauro@osn.global>
2020-05-21 12:54:44 -0300

    Made the max_value elixir function work

--- elixir/day1/medium.exs
+++ elixir/day1/medium.exs
@@ -6,4 +6,29 @@ defmodule Recursive do
 		[_ | remaining_list] = list
 		1 + list_size(remaining_list)
 	end
+
+
+	def max_value([]) do
+		nil
+	end
+	def max_value(list) do
+		[max | rest] = list
+		max_value(rest, max)
+	end
+	defp max_value([], max) do
+		max
+	end
+	defp max_value(list, max) do
+		[first | rest] = list 
+		if first > max do
+			max_value(rest, first)
+		else 
+			max_value(rest, max)
+		end
+	end
 end
+
+IO.puts "Testing max_value function."
+IO.puts Recursive.max_value([])
+IO.puts Recursive.max_value([4])
+IO.puts Recursive.max_value([1, 4, 14, 2])
